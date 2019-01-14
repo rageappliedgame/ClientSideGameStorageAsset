c1eb7e1a938ff8883ac435dac0c8be9c897f9ad0
wvdv <wim@vander-vegt.nl>
2019-01-14 15:53:31 +0100

    1) Updated the example update unity batch file. 2) Note: Unity's pdb2mdb is still to old to work (so a mono installation is required).

--- GameStorageClientAsset/updateUnity.cmd
+++ GameStorageClientAsset/updateUnity.cmd
@@ -1,5 +1,5 @@
-cd "C:\Users\veg\Documents\Visual Studio 2015\Projects\GameStorageClientAsset\GameStorageClientAsset\bin\Debug"
-cmd /c "c:\Program Files (x86)\Mono\bin\pdb2mdb.bat" ModelAsset.dll
-cmd /c "c:\Program Files (x86)\Mono\bin\pdb2mdb.bat" RageAssetManager.dll
-cmd /c copy *.dll "C:\Unity\UITests\Assets"
-cmd /c copy *.dll.mdb "C:\Unity\UITests\Assets"
+cd "C:\Rage\ClientSideGameStorageAsset\GameStorageClientAsset\bin\Debug"
+cmd /c "C:\Program Files (x86)\Mono\bin\pdb2mdb.bat" GameStorageClientAsset.dll
+cmd /c "C:\Program Files (x86)\Mono\bin\pdb2mdb.bat" RageAssetManager.dll
+cmd /c copy *.dll "C:\Unity\CliensSideGameStorage Demo\Assets"
+cmd /c copy *.dll.mdb "C:\Unity\CliensSideGameStorage Demo\Assets"
